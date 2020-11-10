aa281846bcb9568b5403cbcb276d5bac116f0304
renight0 <72281567+renight0@users.noreply.github.com>
2020-11-10 12:10:00 -0300

    Initial first person project commit
    Scripts for character and camera movement have been set.

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float mouseSensibility = 100f;
+    public float minAngle = -90f, maxAngle = 90f;
+
+    public Transform transformPlayer;
+
+    float cameraRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensibility * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensibility * Time.deltaTime;
+
+        cameraRotation -= mouseY;
+        cameraRotation = Mathf.Clamp(cameraRotation, minAngle, maxAngle);
+        transform.localRotation = Quaternion.Euler(cameraRotation, 0, 0);
+    }
+}
---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor.UI;
+using UnityEngine;
+
+namespace Movement
+{
+    public class MoveCharacter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public CharacterController control;  
+        public float speed = 6f;
+        public float jumpHeight = 3f;
+        public float characterGravity = -20f;
+        
+        public Transform checkFloor;
+        public float sphereRay = 0.4f;
+        public LayerMask floorMask;
+        public bool onTheFloor;
+        Vector3 speedFall; 
+
+        void Start()
+        {
+            control = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Debug.Log("MoveCharacter Update");
+            onTheFloor = Physics.CheckSphere(checkFloor.position, sphereRay,floorMask);
+
+            if(onTheFloor && speedFall.y < 0)
+            {
+                speedFall.y = -2f;
+            }
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = (transform.right * x + transform.forward * z).normalized;
+
+            control.Move(move * speed * Time.deltaTime);
+
+            if(Input.GetButtonDown("Jump") && onTheFloor)
+            {
+                speedFall.y = Mathf.Sqrt(jumpHeight * -2f * characterGravity);
+                Debug.Log("Jump and onTheFloor");
+            }
+
+            speedFall.y += characterGravity * Time.deltaTime;
+
+            control.Move(speedFall * Time.deltaTime);
+
+            Debug.Log("speedFall.y: " + speedFall.y);
+        }
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(checkFloor.position, sphereRay);
+        }
+        
+        }
+    }
